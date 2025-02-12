391b620696bfb3462c0fadb37130b235d87349bf
vikassharma602 <yuvisharma602@gmial.com>
2025-02-12 12:10:14 +0530

    datatype learn

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId =1444553
+let accountEmail = "vikas@gmail.com"
+var accountPassword = "123455"
+accountCity = 'Jaipur'
+
+//------------------------------
+            //const accountId = 2 not allowed
+//syntax error show 
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121121"
+accountCity = "bengaluru" 
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
+
+
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) we are using nodejs ,not browser
+
+let name = "vikas "
+let age = 18
+let isLoggedIn = false 
+
+//number 
